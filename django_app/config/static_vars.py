78eb71740de8e97f0457253abe557286d07c74d4
ettzzz <39869906+ettzzz@users.noreply.github.com>
2021-08-13 07:50:36 +0800

    Update static_vars.py

--- django_app/config/static_vars.py
+++ django_app/config/static_vars.py
@@ -2,7 +2,7 @@
 
 import os
 
-ROOT = os.getcwd()
+ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 STOCK_HISTORY_PATH = os.path.join(ROOT, 'trade_history.db')
 DAY_ZERO = '2019-01-01'
 UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0'
